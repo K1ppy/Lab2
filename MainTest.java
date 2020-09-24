32dc3c37ac4bdd0b402320f83b5d93a6e06f2900
K1ppy <71040545+K1ppy@users.noreply.github.com>
2020-09-24 11:30:48 +0300

    Update MainTest.java

--- MainTest.java
+++ MainTest.java
@@ -8,10 +8,31 @@ import org.junit.jupiter.api.Test;
 class MainTest {
     @Test
     void testCalcValue() {
-        int n = 3;
-        int m = 3;
-        int actual = Main.minimumColumn(n, m);
-         //не знаю как тестировать рандомные входные данные
-        //не было времени научиться нормально заливать на гитхаб
+        double eps = 1e-2;
+        //Tes1
+        int n = 2;
+        int m = 2;
+        int[][] matrix = new int[n][m];
+        matrix[0][0] = 1;
+        matrix[0][1] = 2;
+        matrix[1][0] = 0;
+        matrix[1][1] = 2;
+        int actual = Main.minimumColumn(matrix, n, m);
+        int expected = 1;
+        assertEquals(expected, actual, eps);
+        //Test2
+        n = 2;
+        m = 3;
+        int[][] new_matrix = new int[n][m];
+        new_matrix[0][0] = 2;
+        new_matrix[0][1] = 2;
+        new_matrix[0][2] = 2;
+        new_matrix[1][0] = 1;
+        new_matrix[1][1] = 2;
+        new_matrix[1][2] = 2;
+        actual = Main.minimumColumn(new_matrix, n, m);
+        expected = 1;
+        assertEquals(expected, actual, eps);
+
     }
 }
